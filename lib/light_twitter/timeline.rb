907174cf09757add2443bab9d478c769ed4ab9fc
Kazuya Hatanaka <ivalice1218@gmail.com>
2017-06-08 10:00:22 +0900

    modify timeline methods to using block version

--- lib/light_twitter/timeline.rb
+++ lib/light_twitter/timeline.rb
@@ -1,25 +1,41 @@
 module LightTwitter
   module Timeline
-    def home_timeline
-      content("home")
+    def home_timeline(&block)
+      content("home", &block)
     end
 
-    def mentions_timeline
-      content("mentinos")
+    def mentions_timeline(&block)
+      content("mentinos",&block)
     end
 
-    def user_timeline
-      content("user")
+    def user_timeline(&block)
+      content("user",&block)
     end
 
     private
-      def content(str)
+      def content(str,&block)
+
         response = @endpoint.get("https://api.twitter.com/1.1/statuses/#{str}_timeline.json") 
+        timelines = []
+
         JSON.parse(response.body).each do |twieet_info|
-          puts twieet_info["user"]["name"]
-          puts twieet_info["text"]
-          puts 
+          timelines << [twieet_info["user"]["name"], twieet_info["text"]]
         end
+
+        if block_given?
+          timelines.each do |timeline|
+            yield(*timeline)
+          end
+
+        else
+          timelines.each do |timeline|
+            puts timeline[0]
+            puts timeline[1]
+            puts 
+          end
+        end
+
+
       end
   end
 end
