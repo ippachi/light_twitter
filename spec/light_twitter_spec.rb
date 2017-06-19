f4db5954f9a971b98a22ddec8aa17a073e7c681a
Kazuya Hatanaka <ivalice1218@gmail.com>
2017-06-19 20:49:07 +0900

    remove keys from spec file

--- spec/light_twitter_spec.rb
+++ spec/light_twitter_spec.rb
@@ -1,12 +1,8 @@
 require "spec_helper"
-
-CONSUMER_KEY = "ifVtTNrqg5bexhAcKeI8WBxEK"
-CONSUMER_SECRET = "qhHckNy69xWYfMk2VJFKPJUtZNyLt1A5oftHaDF1GKBDYBbIEC"
-ACCESS_TOKEN = "2503730593-E7ZYDv83lWnAExs8OjNt8jCzFnf8ldu6FUUmq4e"
-ACCESS_SEACRET = "TAYlLQv66ZqV3Kvp95HDOt44RD3jnvXvHAgrzFjqGoGkJ"
+require '../hoge/key'
 
 RSpec.describe LightTwitter do
-  let(:user) {LightTwitter::User.new(CONSUMER_KEY, CONSUMER_SECRET,
+  let(:user) {LightTwitter::User.new(CONSUMER_KEY, CONSUMER_SEACRET,
                                       ACCESS_TOKEN, ACCESS_SEACRET)}
 
   describe "timeline" do
